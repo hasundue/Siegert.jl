87d86a419de449bec80a6d86883e7b2115567861
Shun Ueda <hasundue@gmail.com>
2025-10-30 22:57:15 +0900

    flake: init

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "Experimental Julia implementation of Siegert pseudostates calculation (WIP)";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      git-hooks-nix,
+      treefmt-nix,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            inherit system;
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = false;
+            };
+            lib = nixpkgs.lib // {
+              git-hooks-nix = git-hooks-nix.lib;
+              treefmt-nix = treefmt-nix.lib;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSystem (
+        {
+          pkgs,
+          lib,
+          system,
+        }:
+        let
+          treefmt = lib.treefmt-nix.mkWrapper pkgs {
+            programs.nixfmt = {
+              enable = true;
+            };
+          };
+          git-hooks = lib.git-hooks-nix.${system}.run {
+            src = ./.;
+            hooks = {
+              treefmt = {
+                enable = true;
+                package = treefmt;
+              };
+            };
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              julia
+              treefmt
+            ];
+            shellHook = git-hooks.shellHook;
+          };
+        }
+      );
+    };
+}
